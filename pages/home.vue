3498180f436721d10cdc6065f4372904d1bc0401
osusyami <osusyami@gmail.com>
2021-08-14 12:49:26 +0000

    登録画面のハリポテ実装

--- pages/home.vue
+++ pages/home.vue
@@ -1,43 +1,49 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="500"
-  >
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="500"
+    >
+      <v-container fluid>
+        <v-row dense>
+          <v-col
+            v-for="card in cards"
+            :key="card.title"
+            :cols="card.flex"
+          >
+            <v-card>
+              <v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="card.title"></v-card-title>
+              </v-img>
 
-    <v-container fluid>
-      <v-row dense>
-        <v-col
-          v-for="card in cards"
-          :key="card.title"
-          :cols="card.flex"
-        >
-          <v-card>
-            <v-img
-              :src="card.src"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-            >
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
+              <v-card-actions>
+                <v-spacer></v-spacer>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-check-circle-outline</v-icon>
+                </v-btn>
 
-              <v-btn icon>
-                <v-icon>mdi-check-circle-outline</v-icon>
-              </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-bookmark</v-icon>
+                </v-btn>
 
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
 
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+    <nuxt-link
+      to="/mountain/register" 
+    >新しい山を登録する</nuxt-link>
+
+  </div>
 </template>
 
 <script>
---
+++ pages/mountain/register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p>山登録画面</p>
+
+    <div>
+      <div class="form_base">
+        <div>
+          山名：<input v-model='mountain_name' placeholder='山名'>
+        </div>
+
+        <v-btn
+          @click='add'
+          elevation="2"
+        >登録</v-btn>
+      </div>
+
+
+    </div>
+
+
+
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      
+      }
+    },
+    methods: {
+      add (){
+        alert("登録完了（嘘です。まだ未実装です）");
+      }
+    }
+  }
+</script>
+
+
+<style>
+  .form_base {
+    border-radius: 2%;
+    background-color: whitesmoke;
+    color: black;
+    padding: 2rem;
+  }
+</style>
